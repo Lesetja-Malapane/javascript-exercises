d63b0f11d762256001f533313bd1230ae3f6a0ad
Lesetja <kmalapane155@gmail.com>
2025-05-29 12:19:19 +0200

    created a function with parameter: word> that returns a string of the word reversed [add the updated word/variable (reversedWord) to the current character then set it to that variable]

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let reversedWord = '';
 
+    for (let i = 0; i < word.length; i++) {
+        reversedWord = word[i] + reversedWord;
+    }
+    return reversedWord;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
