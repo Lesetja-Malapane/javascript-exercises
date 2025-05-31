5cbb4a5c2170181dae28f1d16381c65480b0e419
Lesetja <kmalapane155@gmail.com>
2025-05-31 16:43:32 +0200

    palondromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const word = getWord(string)
+    const reversedWord = word.split("").reverse().join("");
+    return word === reversedWord
 };
+
+function getWord(word) {
+    return  word.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").split(" ").join("");
+}
 
 // Do not edit below this line
 module.exports = palindromes;
