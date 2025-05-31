ed67468ac074ad1d247050ff45650494e52966df
Lesetja <kmalapane155@gmail.com>
2025-05-31 16:12:39 +0200

    completed functions> used recursion

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(myArray) {
+  let total = 0;
+	for (i= 0; i < myArray.length; i++) {
+    total += myArray[i];
+  }
+  return total
 };
 
-const multiply = function() {
+const multiply = function(myArray) {
+  let total = 1;
+  for (i = 0; i < myArray.length; i++) {
+    total *= myArray[i];
+  }
+  return total
 
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function fact(a) {
+  if( a == 0) {
+    return 1
+  }
+	return a * fact(a - 1)
 };
 
 // Do not edit below this line
