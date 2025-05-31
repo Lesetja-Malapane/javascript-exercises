5ad894e7442af9b9c0c9613fc99dfb38a44e1b26
Lesetja <kmalapane155@gmail.com>
2025-05-31 15:51:27 +0200

    added a function to convert integer to number with one decimal

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const fahr = (temp - 32) * (5 / 9)
+  return roundToOne(fahr);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const cels = (temp * 9/5) + 32
+  return roundToOne(cels);
 };
+
+function roundToOne(num) {
+  return Math.round(num * 10) / 10
+}
 
 // Do not edit below this line
 module.exports = {
