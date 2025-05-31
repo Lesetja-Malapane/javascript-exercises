40b8cd810f63b0396544d71752073e3fee523a58
Lesetja <kmalapane155@gmail.com>
2025-05-31 17:15:47 +0200

    added function, passed tests

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(list) {
+    let titles = [];
+    for (const obj of list) {
+        titles.push(obj.title)
+        console.log(obj);
+        console.log(obj.title);
+    }
+    return titles;
 };
 
 // Do not edit below this line
